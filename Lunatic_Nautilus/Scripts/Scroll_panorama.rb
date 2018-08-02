21367c75e8fd2d6149d8888ce1f62bac173667e9
shinyjiggly <agenttat@gmail.com>
2018-08-02 16:29:47 -0700

    Bus system!
    -the bus can now go to one of three stops!
    -Portsvale added
    -Port town tileset added
    -side circles added
    -random truck generator
    -a panorama script that's actually easy to use
    -a handful of songs added
    -the wheel in map 1 can not teleport the user to any map

--- Lunatic_Nautilus/Scripts/Scroll_panorama.rb
+++ Lunatic_Nautilus/Scripts/Scroll_panorama.rb
@@ -1,140 +1,116 @@
-#_________________________________________________
-# MOG_Scroll Panorama V1.4            
-#_________________________________________________
-# By Moghunter  
-# http://www.atelier-rgss.com
-#_________________________________________________
-# Movimenta os panoramas na horizontal e na vertical.
-# Fade Mode.
-# Movimentos aleatórios.
-#_________________________________________________
-module MOG
-# ID variable that defines the speed horizontally.
-VARPANO_X = 8
-# ID variable that defines the speed vertically.
-VARPANO_Y = 9 
-# Switch ID that activates the Fade Mode.
-PANO_FADE_MODE_SWITCHE_ID = 4 #SWITCH ID 
-# Speed ​​Fade.
-PANO_FADE_SPEED = 3 
+# Autoscrolling Panoramas
+# by RPG Advocate
 
-# Switch ID that activates the Random Mode.
-# The picture moves in random directions.
-PAN_RAND_MOVE_SWITCHE_ID = 5  #SWITCH ID 
-# Time to change direction.
-PAN_RAND_TIME = 2  #SWITCH ID 
-# Switche ID that leaves the FOG with low priority.
-FOG_Z_SWITCH_ID = 16
-#
-FOG_MOTION_SWITCH_ID = 17
-end
 
-#===============================================================================
-# Game_Map
-#===============================================================================
-class Game_Map
-  attr_reader   :pan_ox                  
-  attr_reader   :pan_oy 
-  attr_accessor :panorama_opacity
-#--------------------------------------------------------------------------
-# Setup
-#--------------------------------------------------------------------------  
-alias mog10_setup setup  
-def setup(map_id)
-  @pan_ox = 0
-  @pan_oy = 0   
-  @opa_loop = 0
-  @pan_rand_scroll_x = 0
-  @pan_rand_scroll_y = 0
-  @rand_time_x = 0
-  @rand_time_y = 0
-  @panorama_opacity = 0
-  mog10_setup(map_id)
-end
-#--------------------------------------------------------------------------
-# Update
-#--------------------------------------------------------------------------
-alias mog10_update update
-def update
-  if $game_switches[MOG::PANO_FADE_MODE_SWITCHE_ID] == true
-    if @opa_loop == 0
-    @panorama_opacity -= MOG::PANO_FADE_SPEED
-    elsif @opa_loop == 1
-    @panorama_opacity += MOG::PANO_FADE_SPEED 
-    end
-    if @panorama_opacity < 1
-    @opa_loop = 1
-    elsif @panorama_opacity > 254
-    @opa_loop = 0  
-    end
-  else
-  @panorama_opacity = 255 
+#==============================================================================
+# ** Game_System
+#------------------------------------------------------------------------------
+#  This class handles data surrounding the system. Backround music, etc.
+#  is managed here as well. Refer to "$game_system" for the instance of
+#  this class.
+#==============================================================================
+
+class Game_System
+  #--------------------------------------------------------------------------
+  # * Public Instance Variables
+  #--------------------------------------------------------------------------
+  attr_accessor :autoscroll_x_speed       # panorama horizontal speed
+  attr_accessor :autoscroll_y_speed       # panorama vertical speed
+  #--------------------------------------------------------------------------
+  # * Object Initialization
+  #--------------------------------------------------------------------------
+  alias autopan_initialize initialize
+  def initialize
+    autopan_initialize
+    @autoscroll_x_speed = 0
+    @autoscroll_y_speed = 0
   end
-  if $game_switches[MOG::PAN_RAND_MOVE_SWITCHE_ID] == true 
-      @rand_time_x += 1
-      @rand_time_y += 1
-    if @rand_time_x > 40 * MOG::PAN_RAND_TIME
-    @rand_time_x = 0
-      case rand(2)
-      when 0
-        @pan_rand_scroll_x = 0 
-      when 1
-        @pan_rand_scroll_x = 1
-      end
-    end
-    if @rand_time_y > 40 * MOG::PAN_RAND_TIME
-    @rand_time_y = 0
-      case rand(2)
-      when 0
-        @pan_rand_scroll_y = 0 
-      when 1
-        @pan_rand_scroll_y = 1 
-      end
-    end
-    if @pan_rand_scroll_x == 0 
-      @pan_ox -= $game_variables[MOG::VARPANO_X] 
-    else 
-      @pan_ox += $game_variables[MOG::VARPANO_X]   
-    end 
-    if @pan_rand_scroll_y == 0 
-      @pan_oy -= $game_variables[MOG::VARPANO_Y]  
-    else @pan_rand_scroll_y == 1
-      @pan_oy += $game_variables[MOG::VARPANO_Y]     
-    end   
-  else
-    @pan_ox -= $game_variables[MOG::VARPANO_X] 
-    @pan_oy -= $game_variables[MOG::VARPANO_Y]
-  end
-mog10_update
-end
 end
 
-#===============================================================================
-# Spriteset_Map
-#===============================================================================
+
+
+#==============================================================================
+# ** Spriteset_Map
+#------------------------------------------------------------------------------
+#  This class brings together map screen sprites, tilemaps, etc.
+#  It's used within the Scene_Map class.
+#==============================================================================
+
 class Spriteset_Map
-#--------------------------------------------------------------------------
-# Update
-#--------------------------------------------------------------------------  
-  alias mog10_update update
+  #--------------------------------------------------------------------------
+  # * Object Initialization
+  #--------------------------------------------------------------------------
+  alias autopan_initialize initialize
+  def initialize
+    @scroll_point_x   = 0
+    @scroll_point_y   = 0
+    @scroll_frames_x  = 0
+    @scroll_frames_y  = 0
+    @scroll_speed_x   = $game_system.autoscroll_x_speed
+    @scroll_speed_y   = $game_system.autoscroll_y_speed
+    # The original call
+    autopan_initialize
+  end
+  #--------------------------------------------------------------------------
+  # * Frame Update
+  #--------------------------------------------------------------------------
+  alias autopan_update update
   def update
-    mog10_update
-    @panorama.ox = $game_map.display_x / 8 + $game_map.pan_ox
-    @panorama.oy = $game_map.display_y / 8 + $game_map.pan_oy
-    @panorama.opacity = $game_map.panorama_opacity
-    if $game_switches[MOG::FOG_Z_SWITCH_ID] == true 
-        @fog.z = -500 
-    else
-        @fog.z = 3000
-    end        
-    if $game_switches[MOG::FOG_MOTION_SWITCH_ID] == true
-        @fog.ox = $game_map.display_x / 3 + $game_map.fog_ox
-        @fog.oy = $game_map.display_y / 3 + $game_map.fog_oy        
-    else
-        @fog.ox = $game_map.display_x / 4 + $game_map.fog_ox
-        @fog.oy = $game_map.display_y / 4 + $game_map.fog_oy        
-    end    
-  end  
+    # Apply new scroll speed from $game_system
+    if $game_system.autoscroll_x_speed != @scroll_speed_x
+      @scroll_speed_x = $game_system.autoscroll_x_speed
+    end
+    if $game_system.autoscroll_y_speed != @scroll_speed_y
+      @scroll_speed_y = $game_system.autoscroll_y_speed
+    end
+    # The original call
+    autopan_update
+    # Perform panorama scrolling
+    scroll
+    # Update panorama plane    
+    if $game_system.autoscroll_x_speed == 0
+      if $game_system.autoscroll_y_speed == 0
+        @panorama.ox = $game_map.display_x / 8
+        @panorama.oy = $game_map.display_y / 8
+      end
+    end
+    a = $game_system.autoscroll_x_speed != 0
+    b = $game_system.autoscroll_y_speed != 0
+    if a or b
+      @panorama.ox = @scroll_point_x
+      @panorama.oy = @scroll_point_y
+    end
+  end
+  #--------------------------------------------------------------------------
+  # * Scroll Panorama
+  #--------------------------------------------------------------------------
+  def scroll
+    if @panorama.bitmap!=nil
+      
+    w = @panorama.bitmap.width
+    h = @panorama.bitmap.height
+    @scroll_frames_x += @scroll_speed_x
+    @scroll_frames_y += @scroll_speed_y
+    while @scroll_frames_x >= 8
+      @scroll_frames_x -= 8
+      @scroll_point_x += 1
+    end
+    while @scroll_frames_x <= -8
+      @scroll_frames_x += 8
+      @scroll_point_x -= 1
+    end
+    while @scroll_frames_y >= 8
+      @scroll_frames_y -= 8
+      @scroll_point_y += 1
+    end
+    while @scroll_frames_y <= -8
+      @scroll_frames_y += 8
+      @scroll_point_y -= 1
+    end
+    @scroll_point_x -= w  if @scroll_point_x > w
+    @scroll_point_x += w  if @scroll_point_x < -w
+    @scroll_point_y -= h  if @scroll_point_y > h
+    @scroll_point_y += h  if @scroll_point_y < -h
+    end
+  end
 end
-
-$mog_rgss_scroll_panorama = true
