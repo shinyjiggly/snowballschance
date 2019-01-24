3b65b43fe842cd16e4913874f43320f277918b5f
shinyjiggly <agenttat@gmail.com>
2019-01-24 15:07:54 -0800

    Welcome to the HUD
    -replaced old HUD with a new one that doesn't lag constantly!!
    -added area 2-7
    -camping tileset now has proper translucent color selected

--- Lunatic_Nautilus/Scripts/hud.rb
+++ Lunatic_Nautilus/Scripts/hud.rb
@@ -1,85 +1,138 @@
-# Your Very Own Hud 
-# by Kiriashi
-# (and then mutilated by lavendersiren)
-# http://www.gdunlimited.net/forums/topic/3598-your-very-own-hud/
-# $game_switches[16] is used to show bars
-#$game_switches[12] is used to denote a cutscene
+#========================================================
+# ** Game_Actor
+# Script by MeisMe
+# Graphics added by Peaches
+#------------------------------------------------------------------------------
+# This class handles the actor. It's used within the Game_Actors class
+# ($game_actors) and refers to the Game_Party class ($game_party).
+#==============================================================================
+class Game_Actor
+#--------------------------------------------------------------------------
+# * Get the current EXP
+#--------------------------------------------------------------------------
+def now_exp
+return @exp - @exp_list[@level]
+end
+#--------------------------------------------------------------------------
+# * Get the next level's EXP
+#--------------------------------------------------------------------------
+def next_exp
+return @exp_list[@level+1] > 0 ? @exp_list[@level+1] - @exp_list[@level] : 0
+end
+end
 
-class Window_YourHUD < Window_Base
-  def initialize
-    super(0, 400, 640, 100)
-    self.opacity = 0
-    self.contents = Bitmap.new(640 - 32, 100 - 32)
-    @counter=6
-    refresh
+#==============================================================================
+# ** Window_HUD
+#------------------------------------------------------------------------------
+# This class is used to display HP, SP and Gold on the map.
+#==============================================================================
+class Window_HUD < Window_Base
+#--------------------------------------------------------------------------
+# * Object Initialization
+#--------------------------------------------------------------------------
+def initialize
+super(-16, 400, 680, 150)
+self.contents = Bitmap.new(width-32, height-32)
+self.opacity = 0
+self.contents.font.size = $defaultfontsize
+self.contents.font.name = $defaultfonttype
+@actors = []
+@old_hp = []
+@old_sp = []
+@old_exp = []
+@old_level = []
+for i in 0...$game_party.actors.size
+@actors.push($game_party.actors[i])
+@old_hp.push(@actors[i].hp)
+@old_sp.push(@actors[i].sp)
+@old_exp.push(@actors[i].now_exp)
+@old_level.push(@actors[i].level)
+end
+@old_gold = $game_party.gold
+refresh
+end
+#--------------------------------------------------------------------------
+# * Refresh
+#--------------------------------------------------------------------------
+def refresh
+
+self.contents.clear
+self.contents.font.color = normal_color
+bitmap = RPG::Cache.picture("hudback")
+self.contents.blt(0, 0, bitmap, Rect.new(0, 0, 640, 108))
+x = 32
+  for i in 0...@actors.size
+    y = 6
+    draw_actor_name(@actors[i], x+58, -4)
+    draw_actor_hp  (@actors[i], x, 15,width = 144)
+    draw_actor_sp  (@actors[i], x, 35, width = 144)
+    x += 150
   end
-  
-  def refresh
+#anything else you want to draw
 
-  if $game_switches[12]==false #if not during a cutscene
+end
+#--------------------------------------------------------------------------
+# * Frame Update
+#--------------------------------------------------------------------------
+def update
+super
 
-    
-    if Input.trigger?(Input::X)
-      if $game_switches[16] == true
-      $game_switches[16] = false
-      else
-      $game_switches[16] = true
-      end
+if $game_switches[16]==true and $game_switches[12]==false 
+  self.visible=true
+else #new
+  self.visible=false
+  #erase everything
+end 
+
+  if @actors.size != $game_party.actors.size #if actor amount has changed
+  @actors = []
+    for i in 0...$game_party.actors.size #update the actor count
+    @actors.push($game_party.actors[i])
+    end
+  refresh #then start drawing
+  return
+  end
+  for i in 0...@actors.size
+    if @old_hp[i] != @actors[i].hp or
+    @old_sp[i] != @actors[i].sp or
+    @old_exp[i] != @actors[i].now_exp or
+    @old_level[i] != @actors[i].level or
+    @old_gold != $game_party.gold
+    refresh
+    @old_hp[i] = @actors[i].hp
+    @old_sp[i] = @actors[i].sp
+    @old_exp[i] = @actors[i].now_exp
+    @old_level[i] = @actors[i].level
+    @old_gold = $game_party.gold
     end
   end
-     if @counter > 5 #counter thingy
-    @counter=0 #then reset it and let stuff happen 
-    self.contents.clear
-    reset_variables   #wipe everything off
-      
-    if $game_switches[16]==true and $game_switches[12]==false 
-      #if bars are meant to be shown and it isn't a cutscene
-      
-    return if !@actor #draw fucking everything
-    for i in 0...$game_party.actors.size
-    x = i *150
-    y = 2
-    @actor = $game_party.actors[i]
-    #draw_actor_graphic(@actor, 20 , 20)
-    draw_actor_name(@actor, x+58, -4)
-    draw_actor_hp(@actor, x, 15,width = 144)
-    draw_actor_sp(@actor, x, 35, width = 144)
-  end
-  self.opacity = 128
-else
-   self.opacity = 0
- end
-else
-  @counter+=1
- end #new 
 end
 
-  def reset_variables
-    @actor = $game_party.actors[0]
-    @old_hp = @actor ? @actor.hp : 0
-    @old_maxhp = @actor ? @actor.maxhp : 0
-    @old_sp = @actor ? @actor.sp : 0
-    @old_maxsp = @actor ? @actor.maxsp : 0
-  end
-  def update
-    super
-    refresh if (@actor = $game_party.actors[0] or
-                @old_hp = @actor ? @actor.hp : 0 or
-                @old_maxhp = @actor ? @actor.maxhp : 0 or
-                @old_sp = @actor ? @actor.sp : 0 or
-                @old_maxsp = @actor ? @actor.maxsp : 0)
-  end
 end
+#========================================================
+# ** Scene_Map
+#------------------------------------------------------------------------------
+# This class performs map screen processing.
+#==============================================================================
 class Scene_Map
-  alias yourhud_main main
-  alias yourhud_update update
-  def main
-    @yourhud = Window_YourHUD.new
-    yourhud_main
-    @yourhud.dispose
-  end
-  def update
-    @yourhud.update
-    yourhud_update
-  end
+#--------------------------------------------------------------------------
+# * Object Aliasing
+#--------------------------------------------------------------------------
+alias hud_scene_map_main main
+alias hud_scene_map_update update
+#--------------------------------------------------------------------------
+# * Object Initialization
+#--------------------------------------------------------------------------
+def main
+@HUD = Window_HUD.new
+hud_scene_map_main
+@HUD.dispose
+end
+#--------------------------------------------------------------------------
+# * Frame Update
+#--------------------------------------------------------------------------
+def update
+@HUD.update
+hud_scene_map_update
+end
 end
